3091dc905a63ba0afcaa61a1210408162519b492
VD_Styles <vdovhan23@gmail.com>
2021-03-21 13:14:51 +0200

    ;init

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+import Wrapper from "../../hoc/Wrapper";
+import Burger from "../../components/Burger/Burger";
+
+
+class BurgerBuilder extends Component {
+    render() {
+        return(
+            <Wrapper>
+                <div>
+                    <Burger/>
+                </div>
+                <div>
+                    Build Control
+                </div>
+            </Wrapper>
+        );
+    }
+}
+
+export default BurgerBuilder;
